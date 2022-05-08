1830bb3160f94f35df6f7f8c309542a77611282c
Jaewon-Yun <woin2ee@gmail.com>
2022-05-08 19:56:59 +0900

    TableView DataSource 구현

--- WeatherCalendar/ViewController.swift
+++ WeatherCalendar/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import FSCalendar // https://github.com/WenchaoD/FSCalendar
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var todoTable: UITableView!
+    
+    let todoItem = ["One", "Two"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calendar.dataSource = self
+        calendar.delegate = self
+        todoTable.dataSource = self
+        todoTable.delegate = self
+        
+        self.setAppearance(of: calendar.appearance)
+        
+        // weekday 한/영 설정
+        calendar.locale = Locale(identifier: "ko_KR")
+//        calendar.locale = Locale(identifier: "en_EN")
+        
+        
+        
     }
 
+    func setAppearance(of ca: FSCalendarAppearance) {
+        ca.headerTitleColor = .red
+        ca.weekdayTextColor = .red
+        
+        ca.eventSelectionColor = .green
+        ca.eventDefaultColor = .yellow
+        
+        ca.selectionColor = .brown
+        ca.todayColor = .blue
+        
+        ca.todaySelectionColor = .red
+        
+        ca.headerDateFormat = "M월"
+        
+        ca.headerMinimumDissolvedAlpha = 0.0
+    }
 
 }
 
+extension ViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItem.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = todoItem[indexPath.row]
+        
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    
+}
