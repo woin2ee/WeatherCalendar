50be9504699381917e48db1481157e945030b369
Jaewon <woin2ee@gmail.com>
2022-05-08 23:38:23 +0900

    Youtube 영상 참고) Collection View 이용하여 Calendar 구현
    https://youtu.be/abbWOYFZd68
    
    각 Month에 맞춰서 날짜 표시하는 Helper부분 구현 X

---
+++ WeatherCalendar/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  WeatherCalendar
+//
+//  Created by Jaewon on 2022/05/08.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
--- WeatherCalendar/ViewController.swift
+++ WeatherCalendar/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(selectedDate)
+        print(collectionView.frame.size.width)
+        
+        setCellsView()
+        
+        totalSquares.removeAll()
+        for i in 1...20 {
+            totalSquares.append(String(i))
+        }
     }
 
-
+    func setCellsView() {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    @IBAction func previousMonth(_ sender: Any) {
+    }
+    @IBAction func nextMonth(_ sender: Any) {
+    }
+    
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return totalSquares.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        
+        return cell
+    }
+}
