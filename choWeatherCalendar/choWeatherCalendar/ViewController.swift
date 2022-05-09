c349f77de00ca6b06dcca807f44e9bec3f0ff11d
chozjjae <ch980428@naver.com>
2022-05-09 23:33:51 +0900

    hi_test_WeatherCalender 캘린더 원격으로 test 중입니다 잘 열리나 확인한번 해주세요

---
+++ choWeatherCalendar/choWeatherCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choWeatherCalendar
+//
+//  Created by comsoft on 2022/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
