f66733630b40c6ea67c71c5846585939d874db2e
Jaewon <woin2ee@gmail.com>
2022-05-28 23:49:06 +0900

    SettingsView 에 Table 추가

--- WeatherCalendar/ViewController/SettingsViewController.swift
+++ WeatherCalendar/ViewController/SettingsViewController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    @IBOutlet weak var settingsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        settingsTable.dataSource = self
+        settingsTable.delegate = self
     }
+}
+
+extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "SettingCell")
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = indexPath.description
+        
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    
 }
